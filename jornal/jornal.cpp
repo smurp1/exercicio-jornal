55fdf954edb3da758e58c3e893afe7d6fbe2c05a
smurp1 <samuelrpereira123@gmail.com>
2021-03-18 21:00:29 -0300

    terminei

--- jornal/jornal.cpp
+++ jornal/jornal.cpp
@@ -7,44 +7,44 @@ void jornal::inicializarsprite()
 {
 	if (!gRecursos.carregouSpriteSheet("templatejornal"))
 	{
-		gRecursos.carregarSpriteSheet("templatejornal", "/assets/assets/JornalTemplate.png", 1, 1);
+		gRecursos.carregarSpriteSheet("templatejornal", "../assets/assets/JornalTemplate.png", 1, 1);
 		jornal.setSpriteSheet("templatejornal");
+		jornal.setEscala(0.5, 0.5);
 	}
 }
 
 void jornal::inicializardados()
 {
-	if (gRecursos.carregouFonte("fonte"))
+	if (!gRecursos.carregouFonte("fonte"))
 	{
-		gRecursos.carregarFonte("fonte", "../assets/assets/pixelplay.ttf");
-
+		gRecursos.carregarFonte("fonte", "../assets/assets/pixelplay.ttf",20);
 	}
 
 	titulo.setFonte("fonte");
 	titulo.setString("titulo");
 	titulo.setCor(0, 0, 0);
-	titulo.setEscala(5, 5);
+	titulo.setEscala(3, 3);
 
 	texto.setFonte("fonte");
 	texto.setString("texto texto texto texto texto texto texto texto texto texto texto");
 	texto.setCor(0, 0, 0);
-	texto.setEscala(2, 2);
+	texto.setEscala(1, 1);
 	texto.setLarguraMaxima(100);
 
 	nome.setFonte("fonte");
 	nome.setString("nome");
 	nome.setCor(0, 0, 0);
-	nome.setEscala(5, 5);
+	nome.setEscala(3, 3);
 
-	data.setFonte("Fonte");
+	data.setFonte("fonte");
 	data.setString("29 de fevereiro de 20000");
 	data.setCor(0, 0, 0);
-	data.setEscala(3, 3);
+	data.setEscala(1, 1);
 
 	site.setFonte("fonte");
 	site.setString("site.com");
 	site.setCor(0, 0, 0);
-	site.setEscala(3, 3);
+	site.setEscala(2, 2);
 }
 
 void jornal::desenharjornal()
@@ -55,8 +55,8 @@ void jornal::desenharjornal()
 void jornal::desenharmateria()
 {
 	titulo.desenhar(200, 100);
-	texto.desenhar(50, 200);
+	texto.desenhar(200, 200);
 	data.desenhar(200, 50);
-	site.desenhar(10, 10);
-	nome.desenhar(10, 50);
+	site.desenhar(100, 10);
+	nome.desenhar(200, 50);
 }
